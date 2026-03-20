4fa840b2fe63e7f84eb86c66f468cb2eb083bd1f
noiia <edwin.lecomte31@gmail.com>
2026-03-20 10:46:22 +0100

    feat(core/depends) : adding checkDependencies and Reassort methods for filemeta types

--- core/internal/types/files.go
+++ core/internal/types/files.go
@@ -3,9 +3,11 @@ package types
 import "time"
 
 type FileMeta struct {
-	Path    string
-	Size    int64
-	ModTime time.Time
+	Path        string
+	Size        int64
+	ModTime     time.Time
+	Dependances []string
+	Priority    int
 }
 
 type Diff struct {
